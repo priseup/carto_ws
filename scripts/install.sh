592eb1515afa750ae1c8001811c6b1cae9c2cb25
pangqingfei <pangqingfei@126.com>
2023-08-07 14:50:44 +0800

    fix bug when compile

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,35 +1,39 @@
 #!/bin/bash -e
-set -o errexit
+set -e
+set -x
+
+# any problem, read src/cartographer_ros/docs/source/compilation.rst
 
 sudo apt update
 sudo apt install -y python3-wstool python3-rosdep ninja-build stow
 
 ##### install abseil #############
 cd ~/carto_ws/abseil-cpp
-mkdir build
+mkdir build 
 cd build
-cmake -G Ninja \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
-  -DCMAKE_INSTALL_PREFIX=/usr/local/stow/absl \
-  ..
-ninja
-sudo ninja install
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local/stow/absl .. > /dev/null
+ninja > /dev/null
+sudo ninja install > /dev/null
 cd /usr/local/stow
-sudo stow absl
+sudo stow absl > /dev/null
 
 ##### install protobuf #############
-sudo apt-get install -y autoconf automake libtool curl make g++ unzip
-cd ~/carto_ws/protobuf
-./configure
-make
-make check
-sudo make install
-sudo ldconfig
+#sudo apt-get install -y autoconf automake libtool curl make g++ unzip
+#cd ~/carto_ws/protobuf
+#./configure
+#make
+#make check
+#sudo make install
+#sudo ldconfig
 
 ##### compile cartographer && cartographer_ros
-sudo rosdep init
-rosdep update
+sudo apt install -y python3-pip
+sudo pip install rosdepc
+
 cd ~/carto_ws
+sudo rosdepc init
+rosdepc update
 rosdep install --from-paths src --ignore-src --rosdistro=${ROS_DISTRO} -y
 catkin_make_isolated --install --use-ninja -j3
+
+echo "~/carto_ws/installed_isolated/setup.bash" >> ~/.bashrc
